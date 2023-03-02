8f0516df944b726c282acd68e9aad201fc0a5abc
Jackson Ramos <jackson.ramoos@outlook.com>
2023-03-02 16:42:42 -0300

    desafio divisao

--- curso-java/DesafioAritmetica.java
+++ curso-java/DesafioAritmetica.java
@@ -1,5 +1,12 @@
 public class DesafioAritmetica {
     public static void main(String[] args) {
-        
+        double a, b, numerator, division;
+
+        a = (Math.pow((6*(3+2)),2))/(3*2);
+        b = Math.pow(((1-5)*(2-7)/2),2);
+        numerator = Math.pow((a-b),3);
+        division = numerator/Math.pow(10,3);
+
+        System.out.println("The division results in: "+division);
     }
 }
