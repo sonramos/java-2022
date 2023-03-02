73f7e665e32fe605dfa711bc8cf309ac77acdb70
Jackson Ramos <jackson.ramoos@outlook.com>
2023-03-02 16:25:30 -0300

    desafio String to double

---
+++ curso-java/DesafioAritmetica.java
@@ -0,0 +1,5 @@
+public class DesafioAritmetica {
+    public static void main(String[] args) {
+        
+    }
+}
--- curso-java/DesafioConversao.java
+++ curso-java/DesafioConversao.java
@@ -2,6 +2,18 @@ import java.util.Scanner;
 
 public class DesafioConversao {
     public static void main(String[] args) {
+
+        double salario1, salario2, salario3;
         Scanner scanner = new Scanner(System.in);
+        System.out.print("Salario 1:");
+        salario1 = Double.parseDouble(scanner.next().replaceAll(",","."));
+        System.out.print("Salario 1:");
+        salario2 = Double.parseDouble(scanner.next().replaceAll(",","."));
+        System.out.print("Salario 1:");
+        salario3 = Double.parseDouble(scanner.next().replaceAll(",","."));
+
+        scanner.close();
+
+        System.out.println("Media dos salarios = "+(salario1+salario2+salario3)/3);
     }
 }
